c19e807ae45c4180678d0abe91191e136d524858
Thispara <paradon.take@gmail.com>
2024-08-29 14:36:37 +0700

    vite.config changed

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main : 'src/main.jsx',
-      }
-    }
-  },
-  server: {
-    historyApiFallback: true
-  }
-});
+})
